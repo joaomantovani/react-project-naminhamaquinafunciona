d9a1eeeeea768bfd4a88f3be58a0443b04fecd19
J <jpm.mantovani45@gmail.com>
2019-11-07 20:53:17 -0300

    feat: add routes

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './../styles/logo.svg';
 import './../styles/App.css';
 
-function App() {
+function About() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First page
+            About page
         </p>
         <a
-          className="App-link"
+          className="About-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          First page
+          About page
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default About;
